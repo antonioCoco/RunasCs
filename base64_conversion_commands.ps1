e74153d87960dfa47bcedf1f5556067b8a7f059c
antonioCoco <a.cocomazzi93@gmail.com>
2019-12-14 03:19:40 +0100

    some tweaks on powershell wrapper

--- base64_conversion_commands.ps1
+++ base64_conversion_commands.ps1
@@ -1,9 +1,3 @@
-#x64
-$Filename = (Get-Location).Path + "\RunasCs_net2_x64.exe"
+$Filename = (Get-Location).Path + "\RunasCs_net2.exe"
 $base64string_x64 = [Convert]::ToBase64String([IO.File]::ReadAllBytes($FileName))
 $base64string_x64 | Out-File RunasCs.base64
-
-#x86
-$Filename = (Get-Location).Path + ".\RunasCs_net2_x86.exe"
-$base64string_x86 = [Convert]::ToBase64String([IO.File]::ReadAllBytes($FileName))
-$base64string_x86 | Out-File RunasCs_x86.base64
